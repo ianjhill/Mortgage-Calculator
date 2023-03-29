672f5349c5d2c3c5b41dd120b6afc4fb0ef95179
ianjhill <ianjhill1@gmail.com>
2023-03-29 13:59:26 -0400

    Initial Commit

---
+++ Mortgage Calculator/Mortgage_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mortgage_CalculatorApp.swift
+//  Mortgage Calculator
+//
+//  Created by Ian Hill on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Mortgage_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
